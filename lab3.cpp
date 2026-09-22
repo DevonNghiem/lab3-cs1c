246bfab423d0ff042522029d948256b0f943a3e9
DevonNghiem <dnghiem2@saddleback.edu>
2026-09-21 18:41:43 -0700

    Has a while loop to check if gpa is valid

--- lab3.cpp
+++ lab3.cpp
@@ -17,6 +17,13 @@ int main()
     cout << "Enter your GPA: ";
     cin >> gpa;
 
+    while (gpa < 0.0 || gpa > 4.0) 
+    {
+        cerr << "Invalid GPA. Please enter a GPA between 0.0 and 4.0." << endl;
+        cout << "Enter your GPA: ";
+        cin >> gpa;
+    } 
+
     cout << "Student ID is: " << studentId << endl;
     cout << "Name is: " << name << endl;
     cout << "GPA is: " << gpa << endl;

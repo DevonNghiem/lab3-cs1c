b6e58393eb2e2a38ece81b118dd3643d0c75e569
DevonNghiem <dnghiem2@saddleback.edu>
2026-09-21 18:53:13 -0700

    Add GPA field (#2)
    * Add GPA field
    
    * Has a while loop to check if gpa is valid

--- lab3.cpp
+++ lab3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     string name;
+    double gpa;
     int studentId;
 
     cout << "Enter your name: ";
@@ -13,8 +14,19 @@ int main()
     cout << "Enter your student ID: ";
     cin >> studentId;
 
+    cout << "Enter your GPA: ";
+    cin >> gpa;
+
+    while (gpa < 0.0 || gpa > 4.0) 
+    {
+        cerr << "Invalid GPA. Please enter a GPA between 0.0 and 4.0." << endl;
+        cout << "Enter your GPA: ";
+        cin >> gpa;
+    } 
+
     cout << "Student ID is: " << studentId << endl;
     cout << "Name is: " << name << endl;
-
+    cout << "GPA is: " << gpa << endl;
+    
     return 0;
 }
